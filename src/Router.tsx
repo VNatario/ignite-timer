72812db091fb1f419a17380f35f2f5982cfa7e29
Vitor Natario <natariovitor@gmail.com>
2023-08-04 17:19:14 -0300

    feat: add DefaultLayout in routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      {/* Aplicando Default layout em todas as rotas que comecem com '/' */}
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
