0c2a2f691fa3705b34609eb36001d7ed7ff44fa1
Vitor Natario <natariovitor@gmail.com>
2023-08-04 16:56:13 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Button } from './components/Buton'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -6,7 +6,7 @@ export const defaultTheme = {
   'gray-400': '#8D8D99',
   'gray-500': '#7C7C8A',
   'gray-600': '#323238',
-  // 'gray-700': '#29292E',
+  'gray-700': '#29292E',
   'gray-800': '#202024',
   'gray-900': '#121214',
 
